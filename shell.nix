b698701c849064ea67a52a660ddc5f461b09be81
Ilya Mateyko <me@astrophena.name>
2022-01-29 18:49:51 +0300

    configure lorri

--- shell.nix
+++ shell.nix
@@ -1,13 +1,10 @@
 { pkgs ? import <nixpkgs> { } }:
 
 pkgs.mkShell {
-  name = "site";
-  packages = with pkgs; [ git go_1_17 goimports nodejs ];
-  shellHook = ''
-    export PS1="$PS1(site) "
-    if [[ ! -d node_modules ]]; then
-      echo "==> Installing Node.js dependencies..."
-      npm install
-    fi
-  '';
+  packages = with pkgs; [
+    git
+    go_1_17
+    goimports
+    nodejs
+  ];
 }
