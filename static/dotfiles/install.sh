f775ba5176e8336b492498066f43fcf9aa606809
Ilya Mateyko <me@astrophena.name>
2022-03-04 18:08:32 +0000

    static/dotfiles/install.sh: sync

--- static/dotfiles/install.sh
+++ static/dotfiles/install.sh
@@ -16,39 +16,32 @@ _() {
 	[[ -z "${DEBUG:-}" ]] || set -x
 
 	no_color='\033[0m'
+	yellow_color='\033[0;33m'
 
 	info() {
-		local yellow_color='\033[0;33m'
 		echo -e "${yellow_color}"$@"${no_color}"
 	}
 
-	error() {
-		local red_color='\033[0;31m'
-		echo -e "${red_color}"$@"${no_color}"
-		exit 1
+	DOTFILES="$HOME/src/dotfiles"
+	[[ "$CODESPACES" == "true" ]] && {
+		# GitHub Codespaces clones dotfiles there.
+		# See https://docs.github.com/en/codespaces/troubleshooting/troubleshooting-dotfiles-for-codespaces.
+		DOTFILES="/workspaces/.codespaces/.persistedshare/dotfiles"
 	}
+	export PATH="$DOTFILES/vendor/rcm/bin:$PATH"
 
-	require_command() {
-		command -v "$1" &>/dev/null || {
-			error ""$2" is required. Install it and try again."
-		}
+	[[ ! -d "$DOTFILES" ]] && {
+		info "==> Cloning Git repository..."
+		git clone -q https://github.com/astrophena/dotfiles "$DOTFILES"
 	}
-
-	[[ -d "$HOME/src/dotfiles" ]] &&
-		error "dotfiles are already installed. Run u to update them."
-
-	require_command "rcup" "rcm"
-	require_command "git" "Git"
-	require_command "vim" "Vim"
-
-	info "==> Cloning Git repository..."
-	git clone -q https://github.com/astrophena/dotfiles "$HOME/src/dotfiles"
 
 	info "==> Installing dotfiles..."
-	RCRC="$HOME/src/dotfiles/rcrc" rcup -f
+	RCRC="$DOTFILES/rcrc" rcup -f
 
-	info "==> Installing Vim plugins..."
-	vim -es -u ~/.vim/vimrc -i NONE -c "PlugInstall" -c "qa"
+	command -v vim &>/dev/null && {
+		info "==> Installing Vim plugins..."
+		vim -es -u ~/.vim/vimrc -i NONE -c "PlugInstall" -c "qa"
+	}
 }
 
 # Now that we know the whole script has downloaded, run it.
