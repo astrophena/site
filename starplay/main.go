d57ceeaf09f0edf5b67d04abbff3278751441e96
Ilya Mateyko <me@astrophena.name>
2025-02-04 15:30:20 +0300

    starplay: add a copyright header
    Signed-off-by: Ilya Mateyko <me@astrophena.name>

--- starplay/main.go
+++ starplay/main.go
@@ -1,3 +1,7 @@
+// © 2024 Ilya Mateyko. All rights reserved.
+// Use of this source code is governed by the ISC
+// license that can be found in the LICENSE file.
+
 //go:build js
 
 // Starplay implements the WebAssembly module that powers https://astrophena.name/starplay.
